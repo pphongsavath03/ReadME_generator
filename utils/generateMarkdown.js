94808972223331309042403bdb4fbeea534cfaea
pphong <pphongsavath03@gmail.com>
2021-09-27 23:02:46 -0400

    update

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -107,7 +107,7 @@ function generateMarkdown(data) {
 
   ## Contacts
 
-  Github Profile: https://github.com/${data.username}
+  Github Profile: https://github.com/${data.github}
   Email: ${data.email}
   
 
